9146ba5097daad6b7b79f7baf7b1b325f20d08da
Тимур <popraeshuu@gmail.com>
2026-08-31 15:40:44 +0300

    Conditional rendering

--- lesson-2/src/App.tsx
+++ lesson-2/src/App.tsx
@@ -1,33 +1,47 @@
+// const tracks = []
+// const tracks = null
+const tracks = [
+  {
+    id: 1,
+    title: "Musicfun soundtrack",
+    url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
+  },
+  {
+    id: 2,
+    title: "Musicfun soundtrack instrumental",
+    url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
+  },
+]
+const selectedTrackId = 1
+ 
 function App() {
-  const tracks = [
-    {
-      id: 1,
-      title: "Musicfun soundtrack",
-      url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
-    },
-    {
-      id: 2,
-      title: "Musicfun soundtrack instrumental",
-      url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
-    },
-    {
-      id: 3,
-      title: "Musicfun soundtrack instrumental",
-      url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
-    },
-    {
-      id: 4,
-      title: "Musicfun soundtrack instrumental",
-      url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
-    },
-  ]
+  if (tracks === null) {
+    return (
+      <div>
+        <h1>Musicfun player</h1>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+ 
+  if (tracks.length === 0) {
+    return (
+      <div>
+        <h1>Musicfun player</h1>
+        <span>No tracks</span>
+      </div>
+    )
+  }
  
   return (
     <>
       <h1>Musicfun player</h1>
       <ul>
         {tracks.map((track) => (
-          <li key={track.id}>
+          <li
+            key={track.id}
+            style={{ border: track.id === selectedTrackId ? "1px solid orange" : "none" }}
+          >
             <div>{track.title}</div>
             <audio controls src={track.url}></audio>
           </li>
@@ -36,5 +50,4 @@ function App() {
     </>
   )
 }
- 
 export default App
